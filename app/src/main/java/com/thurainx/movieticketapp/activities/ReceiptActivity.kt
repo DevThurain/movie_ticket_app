a87439943fdb747d53c7b8f3c4305739a4874bfb
thurain <thurainbowin.dev@gmail.com>
2022-06-23 23:13:55 +0630

    - receipt screen

--- app/src/main/java/com/thurainx/movieticketapp/activities/ReceiptActivity.kt
+++ app/src/main/java/com/thurainx/movieticketapp/activities/ReceiptActivity.kt
@@ -2,9 +2,15 @@ package com.thurainx.movieticketapp.activities
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import com.thurainx.movieticketapp.R
+import kotlinx.android.synthetic.main.activity_receipt.*
+
 
 class ReceiptActivity : AppCompatActivity() {
     companion object{
@@ -15,5 +21,27 @@ class ReceiptActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receipt)
+
+        setupListeners()
+        setupQr()
+    }
+
+    private fun setupQr() {
+        val text = "GC12345678"
+        val multiFormatWriter = MultiFormatWriter()
+        try {
+            val bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.CODE_128, 500, 100)
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.createBitmap(bitMatrix)
+            ivReceiptQr.setImageBitmap(bitmap)
+        } catch (e: WriterException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun setupListeners() {
+        ivReceiptClose.setOnClickListener {
+            super.onBackPressed()
+        }
     }
 }
